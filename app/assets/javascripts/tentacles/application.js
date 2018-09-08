b59d35970af6db64bf49a846aac12e3d28cb94f4
braindeaf <contact@robl.me>
2018-09-08 02:21:09 +0100

    fix editing

--- app/assets/javascripts/tentacles/application.js
+++ app/assets/javascripts/tentacles/application.js
@@ -10,4 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require_tree .
+
+$(document).ready(function(){
+  $('textarea').autoResize();
+});
