61d5246b245db0202ed55bf968a3f832597b075c
braindeaf <contact@robl.me>
2019-02-28 20:06:21 +0000

    Behave.js

--- app/assets/javascripts/tentacles/application.js
+++ app/assets/javascripts/tentacles/application.js
@@ -16,6 +16,18 @@
 
 $(document).ready(function(){
   $('textarea').each(function() {
-    autosize($(this))
+    t = this
+    autosize(t)
+    editor = new Behave({
+      textarea: t,
+      replaceTab: true,
+      softTabs: true,
+      tabSize: 2,
+      autoOpen: true,
+      overwrite: true,
+      autoStrip: true,
+      autoIndent: true,
+      fence: false
+    });
   })
 });
